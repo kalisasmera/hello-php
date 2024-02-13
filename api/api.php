99e3a6a2443d8ebb6c461cfd41c45e350e23bab8
frpascua <francisco-jose.pascual@unirioja.es>
2024-02-13 14:49:24 +0100

    a

---
+++ api/api.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700" rel="stylesheet"
+		type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500, 600,700" rel="stylesheet"
+		type="text/css" />
+    <title>PAW</title>
+	<style>
+		body {
+			font-family: "Open Sans", verdana, sans-serif;
+			margin: 10px 30px;
+		}
+	</style>
+  </head>
+  <body>
+   
+		<h1>Ejemplo PHP</h1>
+		<p>
+			Ejemplo mínimo de aplicación escrita en php
+		</p>
+  </body>
+</html>
--- api/index.php
+++ api/index.php
@@ -1,8 +1,16 @@
-<html>
-<body>
-<?php
-	
-	echo "Hello world!";
-?>
-</body>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PAW</title>
+  </head>
+  <body>
+   
+	<?php
+		
+		echo "Hello world!";
+	?>   
+   
+  </body>
 </html>
