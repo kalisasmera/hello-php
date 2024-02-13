61bffffa83223bdc5a77ca7eb1b66d5071711481
frpascua <francisco-jose.pascual@unirioja.es>
2024-02-13 13:08:26 +0100

    Create hello.php

---
+++ api/hello.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?php
+	
+	echo "<h1>Hello world!</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultrices nec nisi nec condimentum. Ut faucibus mauris sodales dolor dapibus fermentum. Sed odio dolor, consequat at interdum a, tristique a magna. In consectetur volutpat consectetur. Phasellus maximus fringilla ligula, nec congue ante ultricies molestie. Proin cursus est eget tincidunt imperdiet. Mauris volutpat magna aliquet, venenatis nisi sed, porta urna. Praesent at velit lobortis, tempus mi sit amet, lacinia ligula. Donec bibendum ex et rutrum lacinia. Aenean in nisi sapien. Proin risus dui, accumsan sit amet vehicula in, egestas a lectus. Nulla urna nisi, euismod in ultrices ac, ultricies in dolor. Proin sodales turpis id neque viverra, a auctor odio accumsan. Duis laoreet porta blandit. Donec scelerisque gravida quam, sed malesuada libero rutrum eu. </p>";
+?>
+</body>
+</html>
